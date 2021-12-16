d3deccad3c49c096e44484da483bf67485478753
Whoami <swzyocowboy@icloud.com>
2021-12-16 17:39:39 +0800

    更新 `./scanDirSync.js`

--- scanDirSync.js
+++ scanDirSync.js
@@ -1,7 +1,7 @@
 import scanDirEachSync from "@iyowei/scan-dir-each-sync";
 import lsDirSync from "@iyowei/ls-dir-sync";
 
-export default function scanDirSync(path, worker = () => {}, raw = false) {
+export default function scanDirSync(path, worker, raw = false) {
   if (Object.prototype.toString.call(worker) === "[object Function]") {
     return scanDirEachSync(path, worker, raw);
   }
